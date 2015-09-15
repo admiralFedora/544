21d8c331fbcf72e4cdd2dc8a18dda65c34d81457
Carlos <512.quadraphonic@gmail.com>
2015-09-15 19:55:00 -0400

    JS now does a request routine to find arduino and to get data.

--- Challenge1/xbeeChat.js
+++ Challenge1/xbeeChat.js
@@ -3,6 +3,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var REQUEST = "REQUEST";
+var IDREQUEST = "IDREQUEST";
+
+var sensors = [];
+// 0 = arduino search; 1 = arduino request
+var mode = 0;
+
+function sensor(id){
+  var temp; // int
+  var alive; // boolean
+  this.id = id; // long
+}
 
 /*Four sensors' data   Initialization */
 var id1 = {temp:0,sum:0,count:0};
@@ -10,8 +22,12 @@ var id2 = {temp:0,sum:0,count:0};
 var id3 = {temp:0,sum:0,count:0};
 var id4 = {temp:0,sum:0,count:0};
 
+function swapMode(){
+  mode = mode + 1;
+}
+
 function SaveData(data){
-    var result = ""; 
+    var result = "";
     var JsonData = JSON.parse(data);  //parse the current data
     /*need calibration*/
     switch(JsonData.id) {
@@ -40,7 +56,33 @@ function SaveData(data){
     	     "id 2:  Curtemp: " + id2.temp + " AveTemp: " + (id2.sum/id2.count).toFixed(2)  + "\n" +
 	     "id 3:  Curtemp: " + id3.temp + " AveTemp: " + (id3.sum/id3.count).toFixed(2)  + "\n" +
 	     "id 4:  Curtemp: " + id4.temp + " AveTemp: " + (id4.sum/id4.count).toFixed(2)  + "\n";
-    return result;    
+    return result;
+}
+
+function addNewSensor(data){
+  var JsonData = JSON.parse(data);
+  array.push(new sensor(JsonData.id));
+}
+
+function requestData(){
+  for(var sen in array){
+    sp.write(REQUEST.concat(sen.id.toString()));
+    setTimeout(function(){healthUpdate(sen)}, 2000);
+  }
+}
+
+function recordData(){
+  var JsonData = JSON.parse(data);
+  for(var sen in array){
+    if(sen.id == JsonData.id){
+      sen.temp = JsonData.temp;
+      break;
+    }
+  }
+}
+
+function healthUpdate(var sen){
+  sen.alive = 0;
 }
 
 var portName = process.argv[2],
@@ -74,10 +116,18 @@ http.listen(3000, function(){
 sp.on("open", function () {
   console.log('open');
   sp.on('data', function(data) {
-    var RecData = SaveData(data);  //String for printing received Data
-    console.log('data received:' + RecData );
-    console.log('Raw data received:' + data );
-    io.emit("chat message",  "Received Data:" + RecData);
+    swtich(mode){
+      case 0:
+        addNewSensor(data);
+        break;
+      case 1:
+        recordData(data);
+        break;
+    }
   });
+  sp.write(IDREQUEST);
+  // give 3 seconds to search for all arduino devices
+  setTimeout(swapMode, 3000);
+  // request data from the sensors every 10 seconds
+  setInterval(requestData, 10000);
 });
-
