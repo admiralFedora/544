4773e5f2d5259c83a624aed5a24c48f32b8c4dd6
Hao <hardys1206@gmail.com>
2015-09-25 14:20:07 -0400

    Upload Challenge2

---
+++ Challenge2/node/chall2.js
@@ -0,0 +1,38 @@
+var serialPort = require('serialport');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host  : 'localhost',
+  user  : 'root',
+  password  : 'root',
+  database  : 'mydb'
+});
+
+connection.connect();
+
+function sensorExist(id, x, y){
+  connection.query('SELECT * FROM `sensors` WHERE `id`=?', [id],function(error, row, fields){
+    if(!error){
+      if(row.length != 0){
+        // do nothing if sensor is found
+      } else {
+        addSensor(id, x, y);
+      }
+    } else {
+      console.log('oh noes\n');
+      return false;
+    }
+  });
+}
+
+function addSensor(id, x, y){
+  connection.query('INSERT INTO `sensors` VALUES (?, ?, ?)', [id, x, y],function(error, row, fields){
+    if(!error){
+      console.log("new sensor was added\n");
+    } else {
+      console.log('oh noes\n');
+      return false;
+    }
+  });
+}
+
+sensorExist(22, 3, 6);
