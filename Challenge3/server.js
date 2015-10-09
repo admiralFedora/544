c12290fe15bc6e65a8006c14120e77a52d869b29
admiralFedora <512.quadraphonic@gmail.com>
2015-10-09 10:14:37 -0400

    New package for xbee decoding

--- Challenge3/server.js
+++ Challenge3/server.js
@@ -1,5 +1,28 @@
 var app = require('express')();
 var SerialPort = require("serialport");
+var xbee = require("xbee-api");
+var xbeeApi = xbee.XBeeAPI({
+  api_mode: 2,
+  module: "Any",
+  raw_frames: false
+});
+var xbeeConst = xbee.constants;
+
+var sp = new SerialPort.SerialPort(process.argv[2], {
+  baudrate:9600,
+  parser:xbeeApi.rawParser()
+});
+
+// sensor object
+function sensor(id){
+  var status;
+  var alive;
+  this.id = id;
+}
+
+var server = app.listen(3000, '0.0.0.0', function(){
+  console.log("listening on *:3000");
+});
 
 app.get('/', function(req, res){
   res.send("hello");
@@ -7,8 +30,8 @@ app.get('/', function(req, res){
 
 app.post('/test', function(req, res){
   res.json({"test":"huh"});
-})
+});
 
-var server = app.listen(3000, '0.0.0.0', function(){
-  console.log("listening on *:3000");
+xbeeApi.on("frame_object", function(frame){
+
 });
