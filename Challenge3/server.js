96e14564fd4f4596e70111083084bac3648252fa
admiralFedora <512.quadraphonic@gmail.com>
2015-10-09 11:16:45 -0400

    Added in code for adding a new sensor and checking on the status

--- Challenge3/server.js
+++ Challenge3/server.js
@@ -13,11 +13,29 @@ var sp = new SerialPort.SerialPort(process.argv[2], {
   parser:xbeeApi.rawParser()
 });
 
+var sensors[];
+
 // sensor object
 function sensor(id){
   var status;
-  var alive;
+  var frameId;
   this.id = id;
+}
+
+function addSensor(id){
+  sensors.push(new sensor(id));
+}
+
+function checkStatus(frame){
+  var sen;
+  for(var i = 0; i < sensors.length; i++){
+    sen = sensors[i];
+    if(frame.remote16 == sen.id
+        && frame.id == sen.frameId && frame.deliveryStatus == 0){
+        // remove dead sensor
+        sensors.splice(i,1);
+    }
+  }
 }
 
 var server = app.listen(3000, '0.0.0.0', function(){
@@ -28,10 +46,19 @@ app.get('/', function(req, res){
   res.send("hello");
 });
 
+// this end point returns the array of active sensors
+app.get('/sensors', function(req, res){
+  res.json(JSON.stringify(sensors));
+});
+
 app.post('/test', function(req, res){
   res.json({"test":"huh"});
 });
 
 xbeeApi.on("frame_object", function(frame){
-
+  if(frame.type == xbeeConst.FRAME_TYPE.NODE_IDENTIFICATION){
+    addSensor(frame.sender16);
+  } else if (frame.type == xbeeConst.FRAME_TYPE.ZIGBEE_TRANSMIT_STATUS){
+    checkStatus(frame);
+  }
 });
