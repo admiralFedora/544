3f6198522fcd45bcba9ea3ff242050c2b597c4a9
Pete Benja <kbenja@bu.edu>
2015-10-13 14:53:12 -0400

    Series 1
    Pete

--- Challenge3/Challenge3.ino
+++ Challenge3/Challenge3.ino
@@ -3,7 +3,7 @@
 XBee xbee = XBee(); // Create an XBee object at the top of your sketch
 XBeeResponse response = XBeeResponse();
 // create reusable response objects for responses we expect to handle 
-ZBRxResponse rx = ZBRxResponse();
+Rx16Response rx = Rx16Response();
 ModemStatusResponse msr = ModemStatusResponse();
 
 int Led = 0;
@@ -57,7 +57,7 @@ void loop() {
     
     xbee.readPacket();
     
-    xbee.getResponse().getZBRxResponse(rx);
+    xbee.getResponse().getRx16Response(rx);
     //Serial.print(rx);
     //Serial.println("in the loop");
     Serial.println(xbee.getResponse().getApiId());
@@ -66,34 +66,37 @@ void loop() {
     {
       
       //Packet is of an LED command type
-      /*if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
         // got a zb rx packet
         
         // now fill our zb rx class
-        xbee.getResponse().getZBRxResponse(rx);
-
+        xbee.getResponse().getRx16Response(rx);
+/*
         if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) 
         {
           // the sender got an ACK
-          flashLed(statusLed, 10, 10);
+       
         } 
         else 
         {
             // we got it (obviously) but sender didn't get an ACK
-            flashLed(errorLed, 2, 20);
+        
         }
             
         // set dataLed PWM to value of the first byte in the data
         analogWrite(Led, rx.getData(0));
+*/
 
       } 
+/*
       //Packet is of a status request type
       else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) 
       {
         xbee.getResponse().getModemStatusResponse(msr);
         // the local XBee sends this response on certain events, like association/dissociation
     
-      }*/
+      }
+*/
     } else if (xbee.getResponse().isError()) {
       //nss.print("Error reading packet.  Error code: ");  
       //nss.println(xbee.getResponse().getErrorCode());
