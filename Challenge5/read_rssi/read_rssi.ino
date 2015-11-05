37b6c9d58a18e019e8f2be229b8498909590cb7e
Carlos <512.quadraphonic@gmail.com>
2015-11-05 15:57:27 -0500

    Some changes

--- Challenge5/read_rssi/read_rssi.ino
+++ Challenge5/read_rssi/read_rssi.ino
@@ -14,32 +14,11 @@ uint8_t payload[] = { 0 }; //Payload Packet
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
-AtCommandRequest dbRequest = AtCommandRequest(dbCmd);
-AtCommandResponse dbResponse = AtCommandResponse();
-
 //Sending Status of the LED
-XBeeAddress64 addr64 = XBeeAddress64();
-ZBTxRequest zbTx;
+XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x0000ffff);
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 
 SoftwareSerial XBee(2, 3); // Rx, Tx
-
-void readDB(){
-  xbee.send(dbRequest);
-
-  if(xbee.readPacket(5000)){
-
-    if(xbee.getResponse().getApiId() == AT_COMMAND_RESPONSE){
-      xbee.getResponse().getAtCommandResponse(dbResponse);
-
-      if(dbResponse.isOk()){
-        if(dbResponse.getValueLength() > 0){
-          Serial.print("RSSI: ");
-          Serial.print(dbResponse.getValue()[0],HEX);
-        }
-      }
-    }
-  }
-}
 
 void setup() 
 { 
@@ -47,34 +26,31 @@ void setup()
   Serial.begin(9600);
   XBee.begin(9600);
   xbee.begin(XBee);
+
+  payload[0] = 8 & 0xff;
 }
 
 // looks for an xbee that just joined the network and starts sending to the last one that joined
 void loop() 
 {
-    xbee.readPacket();
-    
-    if (xbee.getResponse().isAvailable()) //Packet received
-    {
-      Serial.println("Packet Received!");
-      if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
-      {
+    xbee.send(zbTx);
 
-        xbee.getResponse().getZBRxResponse(rx);
-        addr64 = rx.getRemoteAddress64();
-        
-        if(rx.getOption() == ZB_PACKET_ACKNOWLEDGED)
+    if(xbee.readPacket(5000)){
+      if (xbee.getResponse().isAvailable()) //Packet received
+      {
+        Serial.println("Packet Received!");
+        Serial.print("packet type:");
+        Serial.println(xbee.getResponse().getApiId(), HEX);
+        if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
         {
-          //Serial.println();
-          
-          switch(rx.getData(0))
-          {
-            case 5:
-              readDB();
-              break;
+          xbee.getResponse().getZBRxResponse(rx);
+
+          if(rx.getData(0) == 9){
+            Serial.println("I got the data!");
+            Serial.print("Rssi: ");
+            Serial.println(rx.getData(1), HEX);
           }
         }
       }
-    }
-
+  }
 }
