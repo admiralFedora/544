d6d3b6637079c4811e1d6dae3fd980526482c2de
admiralFedora <512.quadraphonic@gmail.com>
2015-11-06 11:08:10 -0500

    Changes for idenifing beacons. Also multiple beacons

--- Challenge5/beacon/beacon.ino
+++ Challenge5/beacon/beacon.ino
@@ -1,13 +1,23 @@
 //SENDING
+#include "EEPROMAnything.h"
 #include <Printers.h>
 #include <XBee.h>
 #include <SoftwareSerial.h>
+
+#define VERSION 2
+
+typedef struct {
+  int version;
+  int id;
+} identification;
+
+identification identity;
 
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 
 uint8_t dbCmd[] = {'D','B'};
-uint8_t payload[] = { 5, 0 }; //Payload Packet
+uint8_t payload[] = { 5, 0, 0 }; //Payload Packet
 
 // create reusable response objects for responses we expect to handle 
 ZBRxResponse rx = ZBRxResponse();
@@ -51,6 +61,22 @@ void setup()
   xbee.begin(XBee);
 
   payload[0] = 9 & 0xff;
+  
+  analogReference(EXTERNAL);
+
+  EEPROM_readAnything(0, identity);
+
+  if(identity.version == VERSION){
+    Serial.print("found old id\n");
+  } else {
+    randomSeed(analogRead(PIN)); // seed value from the thermistor
+	
+    identity.id = random(10000,32767) % 255; // generate a specific ID for this device
+    identity.version = VERSION;
+
+    EEPROM_writeAnything(0, identity);
+    Serial.print("write id\n");
+  }
 }
 
 // looks for an xbee that just joined the network and starts sending to the last one that joined
@@ -71,7 +97,8 @@ void loop()
         
         if(rx.getData(0) == 8){
           Serial.println("Sending data");
-          payload[1] = readDB() & 0xff;
+		  payload[1] = identity.id & 0xff;
+          payload[2] = readDB() & 0xff;
           zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
           xbee.send(zbTx);
         }
--- Challenge5/read_rssi/read_rssi.ino
+++ Challenge5/read_rssi/read_rssi.ino
@@ -4,23 +4,67 @@
 #include <XBee.h>
 #include <SoftwareSerial.h>
 
+#define BEACONS 3
+
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 
 uint8_t dbCmd[] = {'D','B'};
-uint8_t payload[] = { 0 }; //Payload Packet
+uint8_t payload[] = { 0 }; //Payload Packet\
+uint8_t toSend*;
 
 // create reusable response objects for responses we expect to handle 
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
 //Sending Status of the LED
-XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x0000ffff);
-ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+XBeeAddress64 broadcast64 = XBeeAddress64(0x00000000, 0x0000ffff);
+ZBTxRequest requestRSSI = ZBTxRequest(broadcast64, payload, sizeof(payload));
+
+XBeeAddress64 addr64;
+ZBTxRequest sendRRSIs;
 
 SoftwareSerial XBee(2, 3); // Rx, Tx
 
 bool keepReading = true;
+
+void getAndSendRSSI()
+{
+	int i = 1;
+	toSend[0] = 2 & 0xff;
+	while(keepReading)
+	{
+		if(xbee.readPacket(4000))
+		{
+			if (xbee.getResponse().isAvailable()) //Packet received
+			{
+			  Serial.println("Packet Received!");
+			  Serial.print("packet type:");
+			  Serial.println(xbee.getResponse().getApiId(), HEX);
+			  if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+			  {
+				xbee.getResponse().getZBRxResponse(rx);
+
+				if(rx.getData(0) == 9)
+				{
+				  Serial.println("I got the data!");
+				  Serial.print("Rssi: ");
+				  Serial.println(rx.getData(1), HEX);
+				  toSend[i] = rx.getData(1) & 0xff;
+				  toSend[i+1] = rx.getData(2) & 0xff;
+				  i += 2;
+				}
+			  }
+			}
+		} else 
+		{
+			keepReading = false;
+		}
+	}
+	
+	sendRRSIs = ZBTxRequest(addr64, toSend, sizeof(toSend));
+	xbee.send(sendRSSIs);
+}
 
 void setup() 
 { 
@@ -30,32 +74,27 @@ void setup()
   xbee.begin(XBee);
 
   payload[0] = 8 & 0xff;
+  
+  toSend = (uint8_t*) malloc(BEACONS * 2 + 1);
 }
 
 // looks for an xbee that just joined the network and starts sending to the last one that joined
 void loop() 
 {
-    xbee.send(zbTx);
-
-    for(int i = 0; i < 2; i++){
-      if(xbee.readPacket(5000)){
-        if (xbee.getResponse().isAvailable()) //Packet received
-        {
-          Serial.println("Packet Received!");
-          Serial.print("packet type:");
-          Serial.println(xbee.getResponse().getApiId(), HEX);
-          if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
-          {
-            xbee.getResponse().getZBRxResponse(rx);
-  
-            if(rx.getData(0) == 9){
-              Serial.println("I got the data!");
-              Serial.print("Rssi: ");
-              Serial.println(rx.getData(1), HEX);
-            }
-          }
-        }
-      }
-    }
+	xbee.readPacket();
+	if(xbee.getResponse().isAvailable())
+	{
+		if(xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+		{
+			xbee.getResponse().getZBRxResponse(rx);
+			addr64 = rx.getRemoteAddress64();
+			
+			if(rx.getData(0) == 1)
+			{
+				xbee.send(requestRSSI);
+				getAndSendRSSI();
+			}
+		}
+	}
     
 }
