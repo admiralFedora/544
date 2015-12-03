029c46d2f816b7fd13356c5f161d7424453982ff
Carlos <512.quadraphonic@gmail.com>
2015-12-03 15:21:04 -0500

    another test change

--- Challenge7/cppPID/testMotor.cpp
+++ Challenge7/cppPID/testMotor.cpp
@@ -6,28 +6,13 @@
 
 int main(){
 	gpioInitialise();
-	Motor* esc = new Motor(27);
-	Motor* wheel = new Motor(22);
-	
-	esc->writeNoSafe(180);
-	sleep(1);
-	esc->writeNoSafe(0);
-	sleep(1);
-	esc->writeNoSafe(90);
-	sleep(1);
-	
-	wheel->write(82);
-	
+	gpioSetPWMfrequency(22, 50);
+	gpioSetPWMrangee(22, 5000);
 	int val;
 	while(1){
 		cin >> val;
-		gpioServo(22, val);
+		gpioPWM(22, val);
 	}
-	
-	sleep(10);
-	
-	esc->write(90);
-	wheel->write(90);
 	
 	gpioTerminate();
 	return 0;
